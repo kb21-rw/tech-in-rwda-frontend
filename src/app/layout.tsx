12c86e3716b6638fc8a66f2ac7e8cd8ae75b01a3
Laetitia Uwumukiza <uwumukizalaetitia@gmail.com>
2025-02-13 11:19:42 +0200

    refactor: fonts folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,5 @@
-import { avenir } from "@/public/fonts";
+import { arvo, avenir, comfortaa, mulish, roboto } from "@/public/fonts";
 import "./globals.css";
-import { Comfortaa, Mulish, Arvo, Roboto } from "next/font/google";
-
-const comfortaa = Comfortaa({
-  subsets: ["latin"],
-  weight: ["300", "400", "700"],
-  display: "swap",
-  variable: "--font-comfortaa",
-});
-
-const mulish = Mulish({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-  display: "swap",
-  variable: "--font-mulish",
-});
-
-const arvo = Arvo({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-  display: "swap",
-  variable: "--font-arvo",
-});
-
-const roboto = Roboto({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-  display: "swap",
-  variable: "--font-arvo",
-});
 
 export default function RootLayout({
   children,
--- src/public/fonts/index.ts
+++ src/public/fonts/index.ts
@@ -1,4 +1,33 @@
 import localFont from "next/font/local";
+import { Comfortaa, Mulish, Arvo, Roboto } from "next/font/google";
+
+export const comfortaa = Comfortaa({
+  subsets: ["latin"],
+  weight: ["300", "400", "700"],
+  display: "swap",
+  variable: "--font-comfortaa",
+});
+
+export const mulish = Mulish({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  display: "swap",
+  variable: "--font-mulish",
+});
+
+export const arvo = Arvo({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  display: "swap",
+  variable: "--font-arvo",
+});
+
+export const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  display: "swap",
+  variable: "--font-arvo",
+});
 
 export const avenir = localFont({
   src: [
