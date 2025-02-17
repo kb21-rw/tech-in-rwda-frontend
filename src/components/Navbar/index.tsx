41ed21158a318d121fc15ac5e9cdb1e69a2ce0a3
Laetitia Uwumukiza <uwumukizalaetitia@gmail.com>
2025-02-17 15:00:24 +0200

    refactor: project spacing

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -10,8 +10,8 @@ const Navbar = (data: NavbarProps) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="h-screen lg:h-full z-10">
-      <div className="flex flex-col md:flex-row px-7 justify-between lg:px-24 py-6 ">
+    <nav className="h-screen lg:h-full z-10 content-wrapper mx-auto">
+      <div className="flex flex-col md:flex-row justify-between py-6">
         <div className="flex items-center justify-between">
           <Logo url={url} text={text} />
           <MenuIcon isOpen={isOpen} toggle={() => setIsOpen(!isOpen)} />
