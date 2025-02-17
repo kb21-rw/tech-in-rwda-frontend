3c0ab98c85abb8c73ad6974eb9b421610daa74b2
Laetitia Uwumukiza <uwumukizalaetitia@gmail.com>
2025-02-17 14:38:22 +0200

    refactor: navbar menu items

--- src/components/Navbar/MenuDropdown/index.tsx
+++ src/components/Navbar/MenuDropdown/index.tsx
@@ -20,7 +20,7 @@ export const MenuItems = ({ menu, isOpen }: { menu: any; isOpen: boolean }) => {
     <div
       className={`${
         isOpen ? "flex flex-col items-center justify-center py-16.75" : "hidden"
-      } lg:flex lg:flex-row font-roboto gap-7.5 lg:gap-12.5 `}
+      } lg:flex lg:flex-row font-roboto gap-7.5 lg:gap-12.5 text-center`}
     >
       {menu.map((item: any) => (
         <NavbarCard
