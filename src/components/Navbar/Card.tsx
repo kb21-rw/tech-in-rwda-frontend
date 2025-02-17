b8fe3c1223be0ddfedf795bf0a10fec72a060d8a
Laetitia Uwumukiza <uwumukizalaetitia@gmail.com>
2025-02-17 14:26:41 +0200

    refactor: navbar component

--- src/components/Navbar/Card.tsx
+++ src/components/Navbar/Card.tsx
@@ -1,10 +1,14 @@
 import { MenuItem } from "@/types/SiteConfigApi";
 import Link from "next/link";
 
-const NavbarCard = ({ url, text }: MenuItem) => {
+interface NavbarCardProps extends MenuItem {
+  className?: string;
+}
+
+const NavbarCard: React.FC<NavbarCardProps> = ({ url, text, className }) => {
   return (
     <div
-      className={`text-2.5xl lg:text-lg font-normal text-primary lg:text-black`}
+      className={`text-2.5xl lg:text-lg font-normal text-primary lg:text-black ${className}`}
     >
       <Link href={url}>{text}</Link>
     </div>
