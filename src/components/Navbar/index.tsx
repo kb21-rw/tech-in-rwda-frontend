707d3dcf793e0c1287c484fd0c36e8550f1f6e34
Laetitia Uwumukiza <uwumukizalaetitia@gmail.com>
2025-02-19 15:14:37 +0200

    refactor: navbar stylings

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -16,8 +16,8 @@ const Navbar = (data: NavbarData) => {
 
   return (
     <nav
-      className={`w-full z-40 bg-white ${
-        isOpen && "absolute left-0 px-7.2 lg:px-0 lg:static"
+      className={`w-full z-40 ${
+        isOpen && "absolute left-0  bg-white px-7.2 lg:px-0 lg:static"
       }`}
     >
       <div className="flex flex-col lg:flex-row justify-between py-10">
