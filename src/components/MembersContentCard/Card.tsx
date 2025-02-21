4989d7a6e818e3b7e207908433b4b17340269c72
Laetitia Uwumukiza <uwumukizalaetitia@gmail.com>
2025-02-21 18:48:00 +0200

    refactor: members content card component

--- src/components/MembersContentCard/Card.tsx
+++ src/components/MembersContentCard/Card.tsx
@@ -1,20 +1,20 @@
 import Image from "next/image";
 import { MembersCardData } from "@/types/MembersData";
 
-const MembersCard = (data: MembersCardData) => {
+const MembersCard = ({ image, title, description }: MembersCardData) => {
   return (
     <div className="flex-col lg:flex-row flex gap-7.5 lg:gap-10">
       <Image
-        src={data.image.url}
-        alt="members"
-        width={data.image.width}
-        height={data.image.height}
+        src={image.url}
+        alt={image.alternativeText || "members"}
+        width={image.width}
+        height={image.height}
         className="w-full lg:w-45 h-full object-cover"
       />
       <div className="font-mulish">
-        <h4 className="font-bold text-2xl">{data.title}</h4>
+        <h4 className="font-bold text-2xl">{title}</h4>
         <p className="font-normal text-sm lg:text-lg text-primary">
-          {data.description}
+          {description}
         </p>
       </div>
     </div>
