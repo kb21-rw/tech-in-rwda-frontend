52e223acbfe5075a957a69be6aad7c2a66f6f9bb
Laetitia Uwumukiza <uwumukizalaetitia@gmail.com>
2025-02-26 11:25:17 +0200

    refactor: text grid component

--- src/components/TextGrid/index.tsx
+++ src/components/TextGrid/index.tsx
@@ -1,21 +1,15 @@
 const TextGrid = ({
   listItem,
   hasBulletPoints,
-  hasSpacing,
 }: {
   listItem: { id: number; description: string }[];
   hasBulletPoints: boolean;
-  hasSpacing: boolean;
 }) => {
   return (
     <ul
       className={`${
-        hasBulletPoints
-          ? "custom-list gap-y-7.5 text-base lg:text-lg lg:gap-y-11.25"
-          : hasSpacing === true
-          ? "gap-y-7.5 text-base lg:text-lg lg:gap-y-11.25"
-          : "text-base lg:text-2xl"
-      } grid md:grid-cols-2 gap-x-20`}
+        hasBulletPoints && "custom-list text-base lg:text-lg "
+      } grid md:grid-cols-2 gap-x-20 gap-y-7.5 lg:gap-y-11.25`}
     >
       {listItem.map((item) => (
         <li key={item.id} className="text-primary font-mulish font-normal">
