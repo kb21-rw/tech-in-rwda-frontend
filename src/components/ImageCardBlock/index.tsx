94471bb737a96a3346c126b479c75f8c72a583b2
Laetitia Uwumukiza <uwumukizalaetitia@gmail.com>
2025-02-26 10:21:52 +0200

    refactor: image card block comonent

--- src/components/ImageCardBlock/index.tsx
+++ src/components/ImageCardBlock/index.tsx
@@ -1,12 +1,12 @@
-import MembersCard from "./Card";
 import { ImageCardBlockData } from "@/types/ImageCardBlockData";
+import ImageCard from "./Card";
 
 const ImageCardBlock = ({ contributors }: ImageCardBlockData) => {
   return (
     <div className="flex flex-col gap-7.5">
       {contributors.map((contributor, id) => (
         <div key={id}>
-          <MembersCard {...contributor} />
+          <ImageCard {...contributor} />
         </div>
       ))}
     </div>
